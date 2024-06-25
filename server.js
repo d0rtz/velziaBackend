5260e6a08c103fb145f32e118448288b9e2f5171
Diego Ortiz <diegoortizruescas@gmail.com>
2024-06-25 16:36:52 +0200

    server.js fixed

---
+++ server.js
@@ -0,0 +1,38 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import houseRoutes from './routes/houseRoutes.js';
+import { connectDB } from './config/database.js';
+import errorHandler from './middleware/errorHandler.js';
+import fs from 'fs';
+import https from 'https';
+
+// Configuración de variables de entorno
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 4999;
+
+// Middlewares
+app.use(express.json());
+app.use(cors());
+
+// Conexión a la base de datos
+connectDB();
+
+// Rutas
+app.use('/', houseRoutes);
+
+// Middleware de manejo de errores
+app.use(errorHandler);
+
+// Leer los archivos del certificado
+const sslOptions = {
+  key: fs.readFileSync('/etc/letsencrypt/live/dev.velzia.es/privkey.pem'),  // Ruta al archivo de clave
+  cert: fs.readFileSync('/etc/letsencrypt/live/dev.velzia.es/fullchain.pem') // Ruta al archivo de certificado
+};
+
+// Crear el servidor HTTPS
+https.createServer(sslOptions, app).listen(port, '0.0.0.0', () => {
+  console.log(`Server running on https://0.0.0.0:${port}`);
+});
